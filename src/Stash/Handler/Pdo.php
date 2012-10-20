871a0cb33278a1af5ff725024f434f3ef815a4f2
Robert Hafner <tedivm@tedivm.com>
2012-10-20 00:04:33 -0700

    Cleaned up comments a bit.

--- src/Stash/Handler/Pdo.php
+++ src/Stash/Handler/Pdo.php
@@ -28,9 +28,12 @@ class Pdo implements HandlerInterface
     protected $password;
     protected $options = array();
 
+    protected $connection = false;
+
 
     /**
-     * This function should takes an array which is used to pass option values to the handler.
+     * This function should takes an array which is used to pass option values
+     * to the handler.
      *
      * @param array $options
      */
@@ -59,7 +62,8 @@ class Pdo implements HandlerInterface
     }
 
     /**
-     * Empty destructor to maintain a standardized interface across all handlers.
+     * Empty destructor to maintain a standardized interface across all
+     * handlers.
      *
      */
     public function __destruct()
@@ -68,9 +72,10 @@ class Pdo implements HandlerInterface
     }
 
     /**
-     * This function should return the data array, exactly as it was received by the storeData function, or false if it
-     * is not present. This array should have a value for "createdOn" and for "return", which should be the data the
-     * main script is trying to store.
+     * This function should return the data array, exactly as it was received by
+     * the storeData function, or false if itis not present. This array should
+     * have a value for "createdOn" and for "return", which should be the data
+     * the main script is trying to store.
      *
      * @param array $key
      * @return array
@@ -81,10 +86,12 @@ class Pdo implements HandlerInterface
     }
 
     /**
-     * This function takes an array as its first argument and the expiration time as the second. This array contains two
-     * items, "createdOn" describing the first time the item was called and "return", which is the data that needs to be
-     * stored. This function needs to store that data in such a way that it can be retrieced exactly as it was sent. The
-     * expiration time needs to be stored with this data.
+     * This function takes an array as its first argument and the expiration
+     * time as the second. This array contains two items, "createdOn" describing
+     * the first time the item was called and "return", which is the data that
+     * needs to be stored. This function needs to store that data in such a way
+     * that it can be retrieced exactly as it was sent. The expiration time
+     * needs to be stored with this data.
      *
      * @param array $key
      * @param array $data
@@ -97,8 +104,8 @@ class Pdo implements HandlerInterface
     }
 
     /**
-     * This function should clear the cache tree using the key array provided. If called with no arguments the entire
-     * cache needs to be cleared.
+     * This function should clear the cache tree using the key array provided.
+     * If called with no arguments the entire cache needs to be cleared.
      *
      * @param null|array $key
      * @return bool
@@ -119,7 +126,7 @@ class Pdo implements HandlerInterface
     }
 
     /**
-     * This handler is available iff the apc extension is present and loaded on the system.
+     * This handler is available if this handler can be used.
      *
      * @return bool
      */
@@ -128,14 +135,9 @@ class Pdo implements HandlerInterface
 
     }
 
+
     protected function makeKey($key)
     {
 
     }
-
-    protected function getCacheTime($expiration)
-    {
-
-    }
-
 }
